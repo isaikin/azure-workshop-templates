16c7ba24fd6298df504e053dc4ab416e713dfa2b
Dmitrii Gvozdev <Dmitrii_Gvozdev@epam.com>
2018-11-15 22:34:13 +0400

    Initialized with code from previous step

---
+++ 03.Azure Storage/Epam.AzureWorkShop.Labs/Models/Interfaces/INoteModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Epam.AzureWorkShop.Labs.ViewModels;
+
+namespace Epam.AzureWorkShop.Labs.Models.Interfaces
+{
+	public interface INoteModels
+	{
+		Guid Add(NoteCreateVM note);
+		bool Delete(Guid id);
+		IEnumerable<NoteVM> GetAll();
+		NoteVM GetById(Guid id);
+	}
+}
---
+++ 03.Azure Storage/Epam.AzureWorkShop.Labs/Models/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Epam.AzureWorkShop.Entities;
+
+namespace Epam.AzureWorkShop.Labs.Models.Interfaces
+{
+	public interface IRepository<T> where T : BasicItem
+	{
+		Guid Add(T item);
+		IEnumerable<T> GetAll();
+		bool Delete(Guid id);
+		T GetById(Guid id);
+	}
+}
